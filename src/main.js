6e82b0990e24c4f543645e7602599091f6e1f695
FreeGreenBird <alvaronieto2012@hotmail.com>
2023-08-14 23:29:55 -0500

    first commit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import GAuth from 'vue-google-oauth2'
+
+Vue.use(GAuth, {
+  clientId: '133184766686-dgmf3e0vn50llq46929krs00qgf80l35.apps.googleusercontent.com',
+  scope: "email",
+  plugin_name:'App Name that you used in google developer console API'
+
+})
+
 Vue.config.productionTip = false
 
 new Vue({

77e5791ad1d0514e74c637ddc3b5887952f1f4d4
FreeGreenBird <alvaronieto2012@hotmail.com>
2023-08-14 23:34:52 -0500

    empty

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import App from './App.vue'
 import GAuth from 'vue-google-oauth2'
 
 Vue.use(GAuth, {
-  clientId: '133184766686-dgmf3e0vn50llq46929krs00qgf80l35.apps.googleusercontent.com',
+  clientId: '<client-id>',
   scope: "email",
   plugin_name:'App Name that you used in google developer console API'
 
